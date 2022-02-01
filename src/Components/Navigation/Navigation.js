0f12599445435e8f14abe71288d6f6c099a837b1
KYRYLO VOITOVSKYI <kirill.voitovskiy@gmail.com>
2022-02-01 22:04:35 +0200

    update

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { authSelectors } from '../../redux/auth';
+import styles from './Navigation.module.css';
+
+export default function Navigation() {
+  const isAuthenticated = useSelector(authSelectors.getIsLoggedIn);
+  return (
+    <nav>
+      <NavLink
+        to="/"
+        exact
+        className={styles.link}
+        activeClassName={styles.activeLink}
+      >
+        Home page
+      </NavLink>
+
+      {isAuthenticated && (
+        <NavLink
+          to="/contacts"
+          exact
+          className={styles.link}
+          activeClassName={styles.activeLink}
+        >
+          Contacts
+        </NavLink>
+      )}
+    </nav>
+  );
+}
